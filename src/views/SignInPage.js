481c05288f78f1c60e5175751d9cca00636b609d
Hieu Vu <star.hieu.99@gmail.com>
2021-06-25 23:01:56 +0700

    Implement container for sign in and sign up form

--- src/views/SignInPage.js
+++ src/views/SignInPage.js
@@ -1,8 +1,35 @@
 import React from "react";
-import Box from "@material-ui/core/Box";
+import {makeStyles} from "@material-ui/core";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
 
 export const SignInPage = (props) => {
-  return <Box>
-    This is sign in page
-  </Box>
+  const classes = useStyles();
+
+  return <div className={classes.root}>
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justify="center"
+      style={{minHeight: '100vh'}}>
+      <Grid item xs={3}>
+        <Paper style={{justifyContent: 'center', height: 200}} className={classes.paper}>
+          Sign in form
+        </Paper>
+      </Grid>
+    </Grid>
+  </div>
 }
--- src/views/SignUpPage.js
+++ src/views/SignUpPage.js
@@ -1,8 +1,33 @@
 import React from "react";
-import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import {makeStyles} from "@material-ui/core";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
 export const SignUpPage = (props) => {
-  return <Box>
-    This is sign in page
-  </Box>
+  const classes = useStyles();
+  return <div className={classes.root}>
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justify="center"
+      style={{minHeight: '100vh'}}>
+      <Grid item xs={3}>
+        <Paper style={{justifyContent: 'center', height: 200}} className={classes.paper}>
+          Sign up form
+        </Paper>
+      </Grid>
+    </Grid>
+  </div>
 }
