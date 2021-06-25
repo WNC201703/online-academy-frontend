710f9133f45f8cec417b93b52ff99cda7640f0d4
Hieu Vu <star.hieu.99@gmail.com>
2021-06-25 13:00:53 +0700

    Set up layout wrapper

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router} from "react-router-dom";
+import React from "react";
+import AppSnackbarProvider from "./contexts/SnackBarProvider";
+import AuthUserContextProvider from "./contexts/user/AuthUserContextProvider";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <AuthUserContextProvider>
+        <AppSnackbarProvider>
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo"/>
+              <p>
+                Edit <code>src/App.js</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer">
+                Learn React
+              </a>
+            </header>
+          </div>
+        </AppSnackbarProvider>
+      </AuthUserContextProvider>
+    </Router>
   );
 }
 
