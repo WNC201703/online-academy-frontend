88653f31f5ef8b484ff12f649dc8463a3edefec2
Hieu Vu <star.hieu.99@gmail.com>
2021-06-25 15:49:03 +0700

    Create homepage and sample page for reference

--- src/views/Homepage.js
+++ src/views/Homepage.js
@@ -1,9 +1,8 @@
 import React from "react";
-import {TextFields} from "@material-ui/icons";
 import Box from "@material-ui/core/Box";
 
 export const Homepage = (props) => {
   return <Box>
-    <TextFields>Homepage</TextFields>
+    This is home page
   </Box>
 }
