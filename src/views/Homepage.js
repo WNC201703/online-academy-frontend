41bff3d3040d5a167359582c735bda89394b62d3
Hieu Vu <star.hieu.99@gmail.com>
2021-06-27 00:25:09 +0700

    Implement 1st carousel for homepage

--- src/views/Homepage.js
+++ src/views/Homepage.js
@@ -1,8 +1,56 @@
-import React from "react";
-import Box from "@material-ui/core/Box";
+import React, {useState} from "react";
+import Carousel from "react-bootstrap/Carousel";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-export const Homepage = (props) => {
-  return <Box>
-    This is home page
-  </Box>
+export const Homepage = () => {
+  const [index, setIndex] = useState(0);
+  const sampleArray = [
+    {
+      title: " Course title 1",
+      desc: "Course description"
+    },
+    {
+      title: " Course title 2",
+      desc: "Course description 2"
+    }, {
+      title: " Course title 3",
+      desc: "Course description 3"
+    },
+    {
+      title: " Course title 4",
+      desc: "Course description 4"
+    }
+  ]
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+  return (
+    <Container>
+      <Row>
+        <Col/>
+        <Col xs={6}>
+          <Carousel activeIndex={index} onSelect={handleSelect}>
+            {
+              sampleArray.map((item) => {
+                return (<Carousel.Item>
+                  <img
+                    className="w-100 h-100"
+                    src='https://www.apple.com/v/education/home/f/images/overview/college_students__dvn47171w282_medium_2x.jpg'
+                    alt="First slide"
+                  />
+                  <Carousel.Caption>
+                    <h3 style={{color: 'white'}}>{item.title}</h3>
+                    <p style={{color: 'white'}}>{item.desc}</p>
+                  </Carousel.Caption>
+                </Carousel.Item>)
+              })
+            }
+          </Carousel>
+        </Col>
+        <Col/>
+      </Row>
+    </Container>
+  );
 }
