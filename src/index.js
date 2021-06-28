5e422ad4e5f8fd25b38ce2e0e5bf89b2afd2374e
Hieu Vu <star.hieu.99@gmail.com>
2021-06-28 17:56:37 +0700

    Import css

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "react-multi-carousel/lib/styles.css";
 
 ReactDOM.render(
   <React.StrictMode>
