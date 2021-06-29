c12832d5ea3ad0b898ec591ad183aeda70f8fe32
Hieu Vu <hieuvu@floatingcube.com>
2021-06-29 11:55:51 +0700

    Create files for API and local storage

--- src/config/api/index.js
+++
@@ -1,7 +0,0 @@
-import { AXIOS_INSTANCE, FILEUPLOAD_AXIOS_INSTANCE } from './apiconfig';
-
-export const API={
-    signup: (param1, param2) => {
-        return AXIOS_INSTANCE.post('/', { param1, param2});
-      },
-}
