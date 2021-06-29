8f2cce7c7562783de0f81ca32627856a4ba7ccf5
Hieu Vu <hieuvu@floatingcube.com>
2021-06-29 11:56:49 +0700

    Refactor axios config and add auth token to API call

--- src/config/api/apiconfig.js
+++ src/config/api/apiconfig.js
@@ -1,6 +1,7 @@
 import axios from "axios";
+import {getAccessToken} from "../../utils/LocalStorageUtils";
 
-const { CLIENT_ENV = "dev" } = process.env;
+const {CLIENT_ENV = "dev"} = process.env;
 
 const envInfo = {
   dev: {
@@ -10,13 +11,33 @@ const envInfo = {
 
 export const AXIOS_INSTANCE = axios.create({
   baseURL: envInfo[CLIENT_ENV].BASE_URL,
+  mode: 'cors',
+  cache: 'no-cache',
+  credentials: 'same-origin',
+  redirect: 'follow',
+  referrer: 'no-referrer',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+
+AXIOS_INSTANCE.interceptors.request.use((config) => {
+  const access_token = getAccessToken();
+  config.headers.Authorization = `Bearer ${access_token}`;
+  return config;
+});
+
+AXIOS_INSTANCE.interceptors.response.use(function (response) {
+  return response;
+}, function (error) {
+  return error.response.data;
 });
 
 export const FILEUPLOAD_AXIOS_INSTANCE = axios.create({
   baseURL: envInfo[CLIENT_ENV].BASE_URL,
-  headers: { "Content-Type": "multipart/form-data" }
+  headers: {"Content-Type": "multipart/form-data"}
 })
 
-export const { BASE_URL } = envInfo[CLIENT_ENV];
+export const {BASE_URL} = envInfo[CLIENT_ENV];
 
 export const BASE_URL_LOGIN = envInfo.login;
