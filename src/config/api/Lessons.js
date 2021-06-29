fed0af8cec82be83c4bacc5900f4ad756e38362b
Hieu Vu <hieuvu@floatingcube.com>
2021-06-29 11:56:01 +0700

    Set up API call for lesson

---
+++ src/config/api/Lessons.js
@@ -0,0 +1,17 @@
+import {AXIOS_INSTANCE} from "./apiconfig";
+
+export async function getAllLessons(courseId) {
+  return await AXIOS_INSTANCE.get(`/api/courses/${courseId}/lessons`);
+}
+
+export async function addLesson(courseId, data) {
+  return await AXIOS_INSTANCE.post(`/api/courses/${courseId}/lessons`, data);
+}
+
+export async function updateLesson(courseId, data, lessonNumber) {
+  return await AXIOS_INSTANCE.post(`/api/courses/${courseId}/lessons/${lessonNumber}`, data);
+}
+
+export async function getLessonByNumber(courseId, data, lessonNumber) {
+  return await AXIOS_INSTANCE.get(`/api/courses/${courseId}/lessons/${lessonNumber}`);
+}
