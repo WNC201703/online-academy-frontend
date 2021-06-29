d7b0f21f5156d11ac12f7990c27d6377dc55f566
Hieu Vu <star.hieu.99@gmail.com>
2021-06-29 16:18:30 +0700

    Add validate name

--- src/utils/ValidationHelper.js
+++ src/utils/ValidationHelper.js
@@ -2,3 +2,8 @@ export const isValidEmail = (email) => {
     const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return pattern.test(String(email).toLowerCase());
 }
+
+export const isValidName = (name) => {
+    const pattern = /^[A-Za-z\s]+$/;
+    return pattern.test(String(name));
+}
