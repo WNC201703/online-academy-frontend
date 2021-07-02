084c624a85387fde169f5964104813fcd9627782
Hieu Ho <55455388+qqq12598@users.noreply.github.com>
2021-07-02 11:01:03 +0700

    Create confirmation dialog to delete course

--- src/views/Admin/CourseManagementPage.js
+++ src/views/Admin/CourseManagementPage.js
@@ -1,49 +1,15 @@
 import React, { useEffect, useState } from "react";
-import { withStyles, useTheme, makeStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import { Table, TableBody, TableCell, TableFooter, TableRow, TablePagination, TableHead } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
-import CircularProgress from "@material-ui/core/CircularProgress";
 import { useSnackbar } from "notistack";
-import PropTypes from 'prop-types';
-import IconButton from '@material-ui/core/IconButton';
-
-import FirstPageIcon from '@material-ui/icons/FirstPage';
 import CreateIcon from '@material-ui/icons/Create';
 import DeleteIcon from '@material-ui/icons/Delete';
-import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
-import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
-import LastPageIcon from '@material-ui/icons/LastPage';
 import { SnackBarVariant } from "../../utils/constant";
 import { getAllCourses, deleteCourse } from "../../config/api/Courses";
-
-const useStyles1 = makeStyles((theme) => ({
-    root: {
-        flexShrink: 0,
-        marginLeft: theme.spacing(2.5),
-    },
-}));
-
-const useStyles = makeStyles((theme) => ({
-    marginAutoContainer: {
-        width: 1000,
-        height: 80,
-        display: 'flex',
-    },
-    marginAutoItem: {
-        margin: 'auto'
-    },
-    root: {
-        display: 'flex',
-        '& > * + *': {
-            marginLeft: theme.spacing(2),
-        },
-        marginTop: 24,
-        marginBottom: 24,
-        alignItems: 'center',
-        justifyContent: 'center',
-
-    }
-}));
+import Rating from '@material-ui/lab/Rating';
+import ConfirmationDialog from "../../components/Dialog/ConfirmationDialog";
+import TablePaginationActions from "@material-ui/core/TablePagination/TablePaginationActions";
 
 const StyledTableCell = withStyles((theme) => ({
     head: {
@@ -63,78 +29,21 @@ const StyledTableRow = withStyles((theme) => ({
     },
 }))(TableRow);
 
-function TablePaginationActions(props) {
-    const classes = useStyles1();
-    const theme = useTheme();
-    const { count, page, rowsPerPage, onChangePage } = props;
-
-    const handleFirstPageButtonClick = (event) => {
-        onChangePage(event, 0);
-    };
-
-    const handleBackButtonClick = (event) => {
-        onChangePage(event, page - 1);
-    };
-
-    const handleNextButtonClick = (event) => {
-        onChangePage(event, page + 1);
-    };
-
-    const handleLastPageButtonClick = (event) => {
-        onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
-    };
-
-    return (
-        <div className={classes.root}>
-            <IconButton
-                onClick={handleFirstPageButtonClick}
-                disabled={page === 0}
-                aria-label="first page"
-            >
-                {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
-            </IconButton>
-            <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="previous page">
-                {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
-            </IconButton>
-            <IconButton
-                onClick={handleNextButtonClick}
-                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-                aria-label="next page"
-            >
-                {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
-            </IconButton>
-            <IconButton
-                onClick={handleLastPageButtonClick}
-                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-                aria-label="last page"
-            >
-                {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
-            </IconButton>
-        </div>
-    );
-}
-
-TablePaginationActions.propTypes = {
-    count: PropTypes.number.isRequired,
-    onChangePage: PropTypes.func.isRequired,
-    page: PropTypes.number.isRequired,
-    rowsPerPage: PropTypes.number.isRequired,
-};
-
-
 export default function ListCourseComponent() {
-    const classes = useStyles();
     const { enqueueSnackbar } = useSnackbar();
     const [loading, setLoading] = useState(false);
-    const [reload, setReload] = useState(0);
     const [courses, setCourses] = useState([]);
     const [page, setPage] = useState(0);
     const [rowsPerPage, setRowsPerPage] = useState(5);
     const [totalResults, setTotalResults] = useState(0);
+    const [openDeleteDialog, setOpenDeleteDialog] = useState({
+        isOpen:false,
+        id:null
+    });
 
     useEffect(() => {
         fetchData();
-    }, [page, rowsPerPage, reload]);
+    }, [page, rowsPerPage]);
 
     useEffect(() => {
     }, [courses]);
@@ -148,22 +57,43 @@ export default function ListCourseComponent() {
         setPage(0);
     };
 
-    const onDeleteCourse = (id) => {
+    const onDeleteCourseClick = (id) => {
+        setOpenDeleteDialog({
+            isOpen:true,
+            id:id
+        });
+    }
+
+    const handleCloseDialog = () => {
+        setOpenDeleteDialog({
+            isOpen:false,
+            id:null
+        });
+    }
+
+    const handleDeleteCourse = () => {
+        const id=openDeleteDialog.id;
+        setOpenDeleteDialog({
+            isOpen:false,
+        });
         deleteCourse(id).then((response) => {
             if (response.status === 204) {
-                // setReload(reload + 1);
                 for (let i = 0; i < courses.length; i++) {
-                        if (courses[i]._id===id) courses.splice(i,1);                    
+                    if (courses[i]._id === id) courses.splice(i, 1);
                 }
-                setTotalResults(totalResults-1);
+                setTotalResults(totalResults - 1);
                 setCourses(courses);
                 enqueueSnackbar("Course deleted successfully", { variant: SnackBarVariant.Success });
             } else {
-
+                enqueueSnackbar("Delete failed", { variant: SnackBarVariant.Error });
             }
+        }).catch((err)=>{
+            enqueueSnackbar("Delete failed", { variant: SnackBarVariant.Error });
         });
 
     }
+
+
     const editUser = (userId) => {
 
     }
@@ -189,66 +119,75 @@ export default function ListCourseComponent() {
     }
 
     return (
-        <div>
-            <Button style={{ marginBottom: 20 }} variant="contained" color="primary" onClick={() => this.addUser()}>
-                Add course
-            </Button>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        <StyledTableCell style={{ width: '10%' }}>#</StyledTableCell>
-                        <StyledTableCell style={{ width: '30%' }}>Name</StyledTableCell>
-                        <StyledTableCell style={{ width: '30%' }}>Category</StyledTableCell>
-                        <StyledTableCell style={{ width: '20%' }}>Teacher</StyledTableCell>
-                        <StyledTableCell style={{ width: '3%' }} align="right">Edit</StyledTableCell>
-                        <StyledTableCell style={{ width: '3%' }} align="right">Delete</StyledTableCell>
-                    </TableRow>
+        <>
+            <div>
+                <Button style={{ marginBottom: 20 }} variant="contained" color="primary" onClick={() => this.addUser()}>
+                    Add course
+                </Button>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <StyledTableCell style={{ width: '10%' }}>#</StyledTableCell>
+                            <StyledTableCell style={{ width: '30%' }}>Name</StyledTableCell>
+                            <StyledTableCell style={{ width: '10%' }}>Rating</StyledTableCell>
+                            <StyledTableCell style={{ width: '20%' }}>Category</StyledTableCell>
+                            <StyledTableCell style={{ width: '20%' }}>Teacher</StyledTableCell>
+                            <StyledTableCell style={{ width: '3%' }} align="right">Edit</StyledTableCell>
+                            <StyledTableCell style={{ width: '3%' }} align="right">Delete</StyledTableCell>
+                        </TableRow>
 
-                </TableHead>
+                    </TableHead>
 
 
-                <TableBody>
-                    {loading ?
-                        <div className={classes.marginAutoContainer}>
-                            <div className={classes.marginAutoItem}>
-                                <CircularProgress />
-                            </div>
-                        </div>
-                        :
-                        courses
-                            .map((row, index) => (
-                                <StyledTableRow key={row._id}>
-                                    <StyledTableCell>{page * rowsPerPage + index + 1}</StyledTableCell>
-                                    <StyledTableCell component="th">{row.name}</StyledTableCell>
-                                    <StyledTableCell >{row.category}</StyledTableCell>
-                                    <StyledTableCell >{row.teacher}</StyledTableCell>
-                                    <StyledTableCell align="right" style={{ color: 'blue' }} onClick={() => editUser(row._id)}><CreateIcon /></StyledTableCell>
-                                    <StyledTableCell align="right" style={{ color: 'red' }} onClick={() => onDeleteCourse(row._id)}><DeleteIcon /></StyledTableCell>
+                    <TableBody>
+                        {loading ?
+                            <StyledTableRow>
+                                {/* <CircularProgress /> */}
+                            </StyledTableRow>
+                            :
+                            courses
+                                .map((row, index) => (
+                                    <StyledTableRow key={row._id}>
+                                        <StyledTableCell>{page * rowsPerPage + index + 1}</StyledTableCell>
+                                        <StyledTableCell component="th">{row.name}</StyledTableCell>
+                                        <StyledTableCell ><Rating precision={0.5} name="read-only" value={row.averageRating / 2} readOnly /></StyledTableCell>
+                                        <StyledTableCell >{row.category}</StyledTableCell>
+                                        <StyledTableCell >{row.teacher}</StyledTableCell>
+                                        <StyledTableCell align="right" style={{ color: 'blue' }} onClick={() => editUser(row._id)}><CreateIcon /></StyledTableCell>
+                                        <StyledTableCell align="right" style={{ color: 'red' }} onClick={() => onDeleteCourseClick(row._id)}><DeleteIcon /></StyledTableCell>
 
-                                </StyledTableRow>
-                            ))}
-                </TableBody>
+                                    </StyledTableRow>
+                                ))}
+                    </TableBody>
 
-                <TableFooter>
-                    <TablePagination
-                        rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
-                        colSpan={5}
-                        count={totalResults}
-                        rowsPerPage={rowsPerPage}
-                        page={page}
-                        SelectProps={{
-                            inputProps: { 'aria-label': 'rows per page' },
-                            native: true,
-                        }}
-                        onChangePage={handleChangePage}
-                        onChangeRowsPerPage={handleChangeRowsPerPage}
-                        ActionsComponent={TablePaginationActions}
-                    />
-                </TableFooter>
-            </Table>
+                    <TableFooter>
+                        <TablePagination
+                            rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                            colSpan={5}
+                            count={totalResults}
+                            rowsPerPage={rowsPerPage}
+                            page={page}
+                            SelectProps={{
+                                inputProps: { 'aria-label': 'rows per page' },
+                                native: true,
+                            }}
+                            onChangePage={handleChangePage}
+                            onChangeRowsPerPage={handleChangeRowsPerPage}
+                            ActionsComponent={TablePaginationActions}
+                        />
+                    </TableFooter>
+                </Table>
 
-        </div>
+            </div>
 
+            <ConfirmationDialog
+                show={openDeleteDialog.isOpen}
+                title='Delete course'
+                detail='Are you sure you want to delete this course?'
+                cancel={() => { handleCloseDialog() }}
+                confirm={() => { handleDeleteCourse() }}
+            ></ConfirmationDialog>
+        </>
 
     );
 }
