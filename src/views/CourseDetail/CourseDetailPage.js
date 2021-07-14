cc025ba9d30382f7538a9b5c23e0024e55d63f9c
Hieu Vu <star.hieu.99@gmail.com>
2021-07-14 15:04:55 +0700

    Separate loading components and handle add favourite course

--- src/views/CourseDetail/CourseDetailPage.js
+++ src/views/CourseDetail/CourseDetailPage.js
@@ -1,19 +1,27 @@
-import React, {useEffect, useState} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import {useSnackbar} from "notistack";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import makeStyles from "@material-ui/core/styles/makeStyles";
-import {blue} from "@material-ui/core/colors";
 import grey from "@material-ui/core/colors/grey";
 import Box from "@material-ui/core/Box";
 import Rating from "@material-ui/lab/Rating";
 import {useParams} from "react-router-dom";
-import {getCourseById, getNewestCourses, getTopViewedCourses} from "../../config/api/Courses";
+import {getCourseById} from "../../config/api/Courses";
 import {SnackBarVariant} from "../../utils/constant";
 import {moneyFormat, ratingNumberFormat} from "../../utils/FormatHelper";
-import Skeleton from "@material-ui/lab/Skeleton";
+import FavoriteBorderOutlinedIcon from '@material-ui/icons/FavoriteBorderOutlined';
+import FavoriteOutlinedIcon from '@material-ui/icons/FavoriteOutlined';
 import {Image} from "semantic-ui-react";
-import {getAllLessons} from "../../config/api/Lessons";
+import {getAllLessons, getRelatedCourse} from "../../config/api/Lessons";
+import {CourseInfoLoading, DescriptionLoading, LessonsLoading, RelatedCourseLoading} from "../../components/Loading";
+import HorizontalCarousel from "../Homepage/HorizontalCarousel";
+import CustomFavouriteOutlinedButton from "../../components/Button/CustomFavouriteOutlinedButton";
+import CustomFavouriteContainedButton from "../../components/Button/CustomFavouriteContainedButton";
+import {addFavouriteCourse, getFavouriteCourse, removeFavouriteCourse} from "../../config/api/User";
+import AuthUserContext from "../../contexts/user/AuthUserContext";
+import CustomPrimaryContainedButton from "../../components/Button/CustomPrimaryContainedButton";
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -70,60 +78,62 @@ const useStyles = makeStyles((theme) => ({
 }));
 export const CourseDetail = () => {
   const classes = useStyles();
+  const {user} = useContext(AuthUserContext);
+  console.log(user);
   const {id} = useParams();
   const {enqueueSnackbar} = useSnackbar();
   const [courseInfo, setCourseInfo] = useState({});
   const [courseLessons, setCourseLessons] = useState([])
+  const [relatedCourses, setRelatedCourses] = useState([])
 
   const [isPending, setIsPending] = useState(false);
-
-  const CourseInfoLoading = () => {
-    return (
-      <Box>
-        <Box className={classes.loading}><Skeleton width="100%" height={250} animation="wave"/></Box>
-        <Box className={classes.loading}><Skeleton width={250} height={100} animation="wave"/></Box>
-        <Box className={classes.loading}><Skeleton animation="wave"/></Box>
-        <Box className={classes.loading}><Skeleton animation="wave"/></Box>
-        <Box className={classes.loading}><Skeleton width={250} animation="wave"/></Box>
-      </Box>)
-  }
-
-  const DescriptionLoading = () => {
-    return (
-      <Box>
-        <Box className={classes.loading}><Skeleton width="100%" height={250} animation="wave"/></Box>
-      </Box>)
-  }
-  const LessonsLoading = () => {
-    return (
-      <Box>
-        
-        <Box className={classes.loading}><Skeleton height={30} animation="wave"/></Box>
-        <Box className={classes.loading}><Skeleton height={25} animation="wave"/></Box>
-        <Box className={classes.loading}><Skeleton height={25} animation="wave"/></Box>
-        <Box className={classes.loading}><Skeleton height={25} animation="wave"/></Box>
-        <Box className={classes.loading}><Skeleton height={25} animation="wave"/></Box>
-        <Box className={classes.loading}><Skeleton height={25} animation="wave"/></Box>
-        <Box className={classes.loading}><Skeleton height={25} animation="wave"/></Box>
-      </Box>)
-  }
-
+  const [isFavourite, setIsFavourite] = useState(false);
+  const [isProcessing, setIsProcessing] = useState(false);
   useEffect(() => {
     const eff = async () => {
       await fetchCourseDetail();
     }
     eff();
-  }, []);
+  }, [id]);
+
+  const handleFavouriteButtonClick = async () => {
+    setIsProcessing(true);
+    const res = await addFavouriteCourse(courseInfo._id);
+    if (res.status === 200) {
+      setIsFavourite(true);
+      enqueueSnackbar("Add favourite course successfully", {variant: SnackBarVariant.Success});
+    } else {
+      enqueueSnackbar("Can not add this course to favourite", {variant: SnackBarVariant.Error});
+    }
+    setIsProcessing(false)
+  }
+
+  const handleRemoveFavouriteCourse = async () => {
+    setIsProcessing(true);
+    const res = await removeFavouriteCourse(courseInfo._id);
+    if (res.status === 204) {
+      setIsFavourite(false);
+      enqueueSnackbar("Remove favourite course successfully", {variant: SnackBarVariant.Success});
+    } else {
+      enqueueSnackbar("Can not remove this course from favourite", {variant: SnackBarVariant.Error});
+    }
+    setIsProcessing(false)
+  }
+
 
   const fetchCourseDetail = async () => {
     setIsPending(true);
     try {
-      const [info, lessons] = await Promise.all([
-        getCourseById(id), getAllLessons(id)
+      const [info, lessons, related, favourite] = await Promise.all([
+        getCourseById(id), getAllLessons(id), getRelatedCourse(id), getFavouriteCourse(user._id)
       ]);
-      console.log(lessons.data);
+      const favouriteIndex = favourite?.data?.findIndex(x => x._id === info?.data?._id);
+      if (!(favouriteIndex < 0)) setIsFavourite(true);
+      console.log(favourite.data);
       setCourseInfo(info.data);
       setCourseLessons(lessons.data);
+      setRelatedCourses(related.data);
+
     } catch (e) {
       enqueueSnackbar("Error, can not get course list", {variant: SnackBarVariant.Error});
       console.log(e);
@@ -155,6 +165,26 @@ export const CourseDetail = () => {
                 <Box className={classes.originMoney}>{moneyFormat(courseInfo?.price)}
                   <span className={classes.discountMoney}>{moneyFormat(120)}</span></Box>
                 <Box className={classes.courseDescription}>{courseInfo.teacher}</Box>
+                <Box className={classes.courseDescription} direction='column'>
+                  {
+                    isFavourite ? <CustomFavouriteContainedButton
+                        size="large"
+                        onClick={handleRemoveFavouriteCourse}
+                        disabled={isProcessing}
+                        startIcon={<FavoriteOutlinedIcon/>}
+                      >
+                        Your favourite course
+                      </CustomFavouriteContainedButton> :
+                      <CustomFavouriteOutlinedButton
+                        onClick={handleFavouriteButtonClick}
+                        size="large"
+                        startIcon={<FavoriteBorderOutlinedIcon/>}
+                      >
+                        Add to favourite
+                      </CustomFavouriteOutlinedButton>
+                  }
+
+                </Box>
               </Box>
             )
           }
@@ -167,25 +197,23 @@ export const CourseDetail = () => {
         <Paper className={classes.paper}>
           <Box className={classes.blockTitle}>Description</Box>
           {
-            isPending ? <DescriptionLoading/> :
-              <Box>{courseInfo.detailDescription}</Box>
-
+            isPending ? <DescriptionLoading/> : <Box>{courseInfo.detailDescription}</Box>
           }
-
-
         </Paper>
         <Paper className={classes.paper}>
           <Box className={classes.blockTitle}>Course Content</Box>
           <Box> {courseLessons?.length} lessons</Box>
           {
             isPending ? <LessonsLoading/> :
-              courseLessons.map(item => {
+              courseLessons?.map(item => {
                 return <Box className={classes.listItem}>{item.lessonNumber} - {item.name}</Box>
-              })
+              }) || 0
           }
         </Paper>
         <Paper className={classes.paper}>
-          <Box className={classes.blockTitle}>Related Courses</Box>
+          {
+            isPending ? <RelatedCourseLoading/> : <HorizontalCarousel title="Related Courses" data={relatedCourses}/>
+          }
         </Paper>
         <Paper className={classes.paper}>
           <Box className={classes.blockTitle}>Ratings</Box>
