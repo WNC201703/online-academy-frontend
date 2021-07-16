e91f715ec0bf6c720165fc21b2a245a732593ca2
Hieu Vu <star.hieu.99@gmail.com>
2021-07-16 15:51:43 +0700

    Create loading component for line list item

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,7 +3,7 @@ import Skeleton from "@material-ui/lab/Skeleton";
 import React from "react";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import grey from "@material-ui/core/colors/grey";
-
+import Paper from "@material-ui/core/Paper";
 
 const useStyles = makeStyles((theme) => ({
   loading: {
@@ -11,6 +11,8 @@ const useStyles = makeStyles((theme) => ({
     background: "transparent"
   }
 }));
+
+const mockArray = [1, 2, 3, 4, 5];
 export const CourseInfoLoading = () => {
   const classes = useStyles();
 
@@ -34,16 +36,15 @@ export const DescriptionLoading = () => {
 
 export const LessonsLoading = () => {
   const classes = useStyles();
-  let arr = [1, 2, 3, 4, 5, 6, 7];
   return <Box> {
-    arr.map(_ => <Box className={classes.loading}><Skeleton height={30} animation="wave"/></Box>)
+    mockArray.map(_ => <Box className={classes.loading}><Skeleton height={30} animation="wave"/></Box>)
   }      </Box>
 }
+
 export const RelatedCourseLoading = () => {
   const classes = useStyles();
-  let arr = [1, 2, 3, 4, 5];
   return <Box display='flex' direction='column' justify={'space-between'}> {
-    arr.map(_ => <Box style={{marginRight: 12}} className={classes.loading}>
+    mockArray.map(_ => <Box style={{marginRight: 12}} className={classes.loading}>
         <Skeleton height={300} width={150} animation="wave"/>
         <Skeleton height={30} width={150} animation="wave"/>
         <Skeleton height={30} width={150} animation="wave"/>
@@ -51,4 +52,24 @@ export const RelatedCourseLoading = () => {
       </Box>
     )
   }      </Box>
+}
+
+export const LineListLoading = () => {
+  return (
+    mockArray.map(_ => <Paper fullwidth
+                              style={{marginBottom: 10}}>
+      <Box display='flex' justify='center' direction={'column'}>
+        <Skeleton style={{marginRight: 12}} height={120} width={100}/>
+        <Box justify='center'>
+          <Skeleton height={30} width={300} animation="wave"/>
+          <Skeleton height={30} width={300} animation="wave"/>
+          <Box display="flex" alignItems="center"
+               justify="center">
+            <Skeleton height={30} width={150} animation="wave"/>
+          </Box>
+          <Skeleton height={30} animation="wave"/>
+        </Box>
+      </Box>
+    </Paper>)
+  );
 }
