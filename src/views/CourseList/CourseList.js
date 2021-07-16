5c17e9ce982fadc0f864ed4c06811eb0d054ac8a
Hieu Vu <star.hieu.99@gmail.com>
2021-07-16 14:14:22 +0700

    Implement course list

---
+++ src/views/CourseList/CourseList.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
+import {useSnackbar} from "notistack";
+import {SnackBarVariant} from "../utils/constant";
+
+export const CourseList = (props) => {
+  const {enqueueSnackbar} = useSnackbar();
+  const handleSuccessButtonClick = () => {
+    enqueueSnackbar("This is a warning message", {variant: SnackBarVariant.Success});
+  }
+  const handleWarningButtonClick = () => {
+    enqueueSnackbar("This is a warning message", {variant: SnackBarVariant.Warning});
+  }
+  const handleErrorButtonClick = () => {
+    enqueueSnackbar("This is a warning message", {variant: SnackBarVariant.Error});
+  }
+
+  return <Box>
+    This is home page
+    <Button
+      onClick={handleSuccessButtonClick}
+      variant="contained" color="primary">
+      Show success message
+    </Button>
+    <Button
+      onClick={handleErrorButtonClick}
+      variant="contained" color="primary">
+      Show error message
+    </Button>
+    <Button
+      onClick={handleWarningButtonClick}
+      variant="contained" color="primary">
+      Show warning message
+    </Button>
+  </Box>
+}
