1633f13574649aa1655a15aaed47855ea7b6940a
Hieu Vu <star.hieu.99@gmail.com>
2021-07-24 09:31:57 +0700

    Handle dropdown nested menu

--- src/components/Menu/DropdownMenu.js
+++ src/components/Menu/DropdownMenu.js
@@ -7,11 +7,15 @@ import ListItemText from '@material-ui/core/ListItemText';
 import WebIcon from '@material-ui/icons/Web';
 import PhoneIphoneIcon from '@material-ui/icons/PhoneIphone';
 import CustomPrimaryContainedButton from "../Button/CustomPrimaryContainedButton";
+import {useHistory} from "react-router-dom";
 
 const StyledMenu = withStyles({
   paper: {
     border: '1px solid #d3d4d5',
   },
+  menuItem: {
+    minWidth: 120
+  }
 })((props) => (
   <Menu
     elevation={0}
@@ -39,16 +43,31 @@ const StyledMenuItem = withStyles((theme) => ({
   },
 }))(MenuItem);
 
-export default function DropdownMenu() {
+export default function DropdownMenu({data}) {
   const [anchorEl, setAnchorEl] = React.useState(null);
+  const [anchorEl2, setAnchorEl2] = React.useState(null);
+
+  const history = useHistory();
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
 
+  const handleItemClicked = (categoryId) => {
+    history.push(`/courses/all/${categoryId}`)
+    handleClose()
+  }
+
   const handleClose = () => {
     setAnchorEl(null);
+    setAnchorEl2(null)
   };
+
+  const handleItemHover = (event) => {
+    setAnchorEl2(event.currentTarget)
+  }
+
+  console.log("category data:", data)
 
   return (
     <div>
@@ -65,21 +84,29 @@ export default function DropdownMenu() {
         anchorEl={anchorEl}
         keepMounted
         open={Boolean(anchorEl)}
-        onClose={handleClose}
-      >
-        <StyledMenuItem>
-          <ListItemIcon>
-            <WebIcon fontSize="small"/>
-          </ListItemIcon>
-          <ListItemText primary="Web Development"/>
-        </StyledMenuItem>
-        <StyledMenuItem>
-          <ListItemIcon>
-            <PhoneIphoneIcon fontSize="small"/>
-          </ListItemIcon>
-          <ListItemText primary="Mobile Development"/>
-        </StyledMenuItem>
+        onClose={handleClose}>
+        {
+          data?.map(item => <StyledMenuItem onMouseEnter={(event) => handleItemHover(event)}
+                                            onClick={() => handleItemClicked(item._id)} style={{minWidth: 120}}>
+            <ListItemText primary={item.name}/>
+          </StyledMenuItem>)
+        }
       </StyledMenu>
+
+      <StyledMenu
+        id="customized-menu"
+        anchorEl={anchorEl2}
+        keepMounted
+        open={Boolean(anchorEl2)}
+        onClose={handleClose}>
+        {
+          data[0]?.childrens?.map(item => <StyledMenuItem onClick={() => handleItemClicked(item._id)}
+                                                          style={{minWidth: 120}}>
+            <ListItemText primary={item.name}/>
+          </StyledMenuItem>)
+        }
+      </StyledMenu>
+
     </div>
   );
 }
