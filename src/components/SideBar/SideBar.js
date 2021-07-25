5af3473648e73354c203554253ca8ee283810ebf
Hieu Vu <star.hieu.99@gmail.com>
2021-07-25 10:55:33 +0700

    Add title for side bar

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,19 +1,14 @@
-import { Menu, ProSidebar, SidebarContent, SubMenu } from "react-pro-sidebar";
-import React, {useMemo, useContext, useEffect, useRef} from "react";
-import { useLocation } from "react-router-dom";
-import useLabels from "../../translations/useLabels";
+import {Menu, ProSidebar, SidebarContent, SubMenu} from "react-pro-sidebar";
+import React, {useEffect, useRef} from "react";
+import {useLocation} from "react-router-dom";
 import {
   BsFillPersonLinesFill,
-  FiShoppingBag,
   FaChartLine
 } from "react-icons/all";
 import makeStyles from "@material-ui/core/styles/makeStyles";
-import AuthUserContext from "../../context/AuthUserContext";
 import SubHeader from "./SubHeader";
 import EnhancedMenuItem from "./EnhancedMenuItem";
-import { admin } from '../../constants/role';
-import { useState } from "react";
-import SidebarContext from "../../context/SidebarContext";
+import {useState} from "react";
 
 const useStyles = makeStyles((theme) => {
   return {
@@ -71,84 +66,49 @@ export default function SideBar() {
   const location = useLocation()
   const [open, setOpen] = useState(false)
   const classes = useStyles();
-  const labels = useLabels();
-  const { user } = useContext(AuthUserContext);
-  const {sidebarToggled, setSidebarToggled} = useContext(SidebarContext);
+  // const {sidebarToggled, setSidebarToggled} = useContext(SidebarContext);
 
   const wrapperRef = useRef(null);
+  // useEffect(() => {
+  //   if (sidebarToggled) {
+  //     function handleClickOutside(event) {
+  //       if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+  //         setSidebarToggled(!sidebarToggled)
+  //       }
+  //     }
+  //
+  //     // Bind the event listener
+  //     document.addEventListener("mousedown", handleClickOutside);
+  //     return () => {
+  //       // Unbind the event listener on clean up
+  //       document.removeEventListener("mousedown", handleClickOutside);
+  //     };
+  //   }
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [wrapperRef, sidebarToggled]);
+  //
+  // const isAdmin = useMemo(() => {
+  //   return user && user.role.includes(admin);
+  // }, [user]);
+
   useEffect(() => {
-    if (sidebarToggled) {
-      function handleClickOutside(event) {
-        if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
-          setSidebarToggled(!sidebarToggled)
-        }
-      }
-
-      // Bind the event listener
-      document.addEventListener("mousedown", handleClickOutside);
-      return () => {
-        // Unbind the event listener on clean up
-        document.removeEventListener("mousedown", handleClickOutside);
-      };
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [wrapperRef, sidebarToggled]);
-
-  const isAdmin = useMemo(() => {
-    return user && user.role.includes(admin);
-  }, [user]);
-
-  useEffect(() => {
-    if(location.pathname.includes('reports')) setOpen(true)
+    if (location.pathname.includes('reports')) setOpen(true)
     else setOpen(false)
-  },[location.pathname])
+  }, [location.pathname])
 
   return (
     <ProSidebar
       className={classes.root}
-      breakPoint="md"
-      toggled={sidebarToggled}>
+      breakPoint="md">
       <SidebarContent ref={wrapperRef} className={classes.content}>
-        <div className={classes.space} />
+        <div className={classes.space}/>
         <Menu>
-          <SubHeader className={classes.subHeader}>{labels.SideBar.salesPipeline}</SubHeader>
+          <SubHeader className={classes.subHeader}>{'Dashboard'}</SubHeader>
           <EnhancedMenuItem
-            icon={<BsFillPersonLinesFill />}
-            linkTo="/customers">
-            {labels.SideBar.customer}
+            icon={<BsFillPersonLinesFill/>}
+            linkTo="/teacher/courses">
+            {'Courses'}
           </EnhancedMenuItem>
-          {
-            isAdmin ?
-              <SubMenu title="Reports" icon={<FaChartLine />} className={classes.subMenu}
-                onClick={() => setOpen(!open)}
-                open={open}>
-                <EnhancedMenuItem
-                  className={classes.subMenuItem}
-                  linkTo="/reports/sales">
-                  {labels.SideBar.sales}
-                </EnhancedMenuItem>
-                <EnhancedMenuItem
-                  className={classes.subMenuItem}
-                  linkTo="/reports/customer">
-                  {labels.SideBar.customerReport}
-                </EnhancedMenuItem>
-              </SubMenu>
-              : <></>
-          }
-          {
-            isAdmin ?
-              <SubHeader className={classes.subHeader}>{labels.SideBar.manage}</SubHeader>
-              : <></>
-          }
-          {
-            isAdmin ?
-              <EnhancedMenuItem
-                icon={<FiShoppingBag />}
-                linkTo="/contracts">
-                {labels.SideBar.deals}
-              </EnhancedMenuItem>
-              : <></>
-          }
         </Menu>
       </SidebarContent>
     </ProSidebar>
