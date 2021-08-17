fcb3025061875bfcc2f1f6b6a455d67ea5420756
Hieu Vu <star.hieu.99@gmail.com>
2021-08-17 22:58:28 +0700

    Handle unfavourite course

--- src/views/CourseList/CourseLineItem.js
+++ src/views/CourseList/CourseLineItem.js
@@ -5,6 +5,11 @@ import {discountFormat, moneyFormat} from "../../utils/FormatHelper";
 import React from "react";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import grey from "@material-ui/core/colors/grey";
+import FavoriteBorderOutlinedIcon from "@material-ui/icons/FavoriteBorderOutlined";
+import CustomFavouriteOutlinedButton from "../../components/Button/CustomFavouriteOutlinedButton";
+import {removeFavouriteCourse} from "../../config/api/User";
+import {SnackBarVariant} from "../../utils/constant";
+import {useSnackbar} from "notistack";
 
 const useStyles = makeStyles((theme) => ({
   itemTitle: {
@@ -28,9 +33,27 @@ const useStyles = makeStyles((theme) => ({
     }
   }
 }));
-export const CourseLineItem = ({item}) => {
+const CourseListType = {
+  ENROLLMENTS: 'enrollments',
+  FAVORITES: 'favorites'
+}
+export const CourseLineItem = ({item, type}) => {
   const classes = useStyles();
   const isDiscount = item?.percentDiscount > 0
+  const {enqueueSnackbar} = useSnackbar();
+
+  const handleRemoveFavouriteCourse = async (e, id) => {
+    const res = await removeFavouriteCourse(id);
+    if (res.status === 204) {
+      window.location.reload();
+
+      enqueueSnackbar("Remove favourite course successfully", {variant: SnackBarVariant.Success});
+    } else {
+      enqueueSnackbar("Can not remove this course from favourite", {variant: SnackBarVariant.Error});
+    }
+  }
+
+
   return (
     <Box padding={2} display='flex' justify='center' direction={'column'}>
       <Image
@@ -42,12 +65,22 @@ export const CourseLineItem = ({item}) => {
         <Box>{item?.shortDescription}</Box>
         <Box display="flex" alignItems="center"
              justify="center">
-          <Rating precision={0.5} name="read-only" value={item?.averageRating/2} readOnly/>
+          <Rating precision={0.5} name="read-only" value={item?.averageRating / 2} readOnly/>
           <span> ({item?.numberOfReviews})</span>
         </Box>
         <Box className={classes.originMoney}>{moneyFormat(item?.price)} {isDiscount ?
           <span
             className={classes.discountMoney}>{moneyFormat(discountFormat(item?.price, item?.percentDiscount))}</span> : <></>}
+        </Box>
+        <Box>
+          {
+            type === CourseListType.FAVORITES ? <CustomFavouriteOutlinedButton
+              onClick={(e) => handleRemoveFavouriteCourse(e, item?._id)}
+              size="large"
+              style={{marginRight: 12}}>
+              Remove from favourite
+            </CustomFavouriteOutlinedButton> : <></>
+          }
         </Box>
       </Box>
     </Box>)
